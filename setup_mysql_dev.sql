4277a4ea3ecaa01faa3527acc232e0de0590c7ff
KisingiMichael <kisingimichael12@gmail.com>
2023-09-20 15:58:36 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- This creates or update the user 'hbnb_dev' with the password 'hbnb_dev_pwd'
 -- The identified by clause specifies the password.
-CREATE OR REPLACE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- This Grant all privileges on the 'hbnb_dev_db' database to 'hbnb_dev'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
