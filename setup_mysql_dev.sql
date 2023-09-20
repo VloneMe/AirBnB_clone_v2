9e0990b4632ae1b614d0b331323ab71c2ed09eec
KisingiMichael <kisingimichael12@gmail.com>
2023-09-20 15:43:58 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- This script prepares a MySQL server for the project:
+
+-- This creates A database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- This creates or update the user 'hbnb_dev' with the password 'hbnb_dev_pwd'
+-- The identified by clause specifies the password.
+CREATE OR REPLACE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- This Grant all privileges on the 'hbnb_dev_db' database to 'hbnb_dev'
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- A Grant SELECT privilege on the 'performance_schema' database to 'hbnb_dev'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
