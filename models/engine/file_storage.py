9e7d1102b8199c8a96906b19fc8a41f88d44c2ac
KisingiMichael <kisingimichael12@gmail.com>
2023-09-20 16:46:22 -0700

    file storage backup

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,37 +5,25 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
 
-    def __init__(self):
-        self.__file_path = 'file.json'
-        self.__objects = {}
-
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage or filtered by class"""
-        if cls is None:
-            return self.__objects
-        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
-
-    def delete(self, obj=None):
-        """Deletes obj from storage if it exists"""
-        if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        serialized_objects = {}
-        for key, obj in self.__objects.items():
-            serialized_objects[key] = obj.to_dict()
-
-        with open(self.__file_path, 'w') as f:
-            json.dump(serialized_objects, f)
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -48,18 +36,15 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
-            with open(self.__file_path, 'r') as f:
-                serialized_objects = json.load(f)
-                for key, obj_dict in serialized_objects.items():
-                    class_name = obj_dict['__class__']
-                    if class_name in classes:
-                        obj = classes[class_name](**obj_dict)
-                        self.__objects[key] = obj
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
