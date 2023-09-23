03612a80e580f0d0440aa24558fcc809e5b6938a
VloneMe <francisntima@yahoo.com>
2023-09-23 01:18:16 -0700

    DBstorage update

--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class User(BaseModel, Base):
+    """
+    This is the class for user
+    """
+    __tablename__ = 'users'
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    """
+    # Relationships with other classes (Place and Review)
+    # using SQLAlchemy's ORM
+    """
+    places = relationship("Place", cascade="all, delete, delete-orphan", backref="user")
+    reviews = relationship("Review", cascade="all, delete, delete-orphan", backref="user")
