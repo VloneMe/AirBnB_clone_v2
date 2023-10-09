d35560deff53582ea427de0f27e17a806f491919
VloneMe <francisntima@yahoo.com>
2023-10-09 07:54:14 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-# Fabric script that generates a .tgz archive from the contents of the web_static
+# Fabric script that generates a .tgz archive
+# from the contents of the web_static
 
 from fabric.api import local
 from datetime import datetime
 import os
+
 
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
     """
     try:
-        # Create the 'versions' folder if it doesn't exist
+        # This creates the 'versions' folder if it doesn't exist
         if not os.path.exists('versions'):
             os.makedirs('versions')
 
@@ -23,10 +25,12 @@ def do_pack():
         # Compress the contents of the 'web_static' folder into the archive
         local("tar -czvf versions/{} web_static".format(archive_name))
 
-        # Return the archive path if the archive has been generated successfully
+        # Return the archive path if the archive has
+        # been generated successfully
         return "versions/{}".format(archive_name)
     except Exception as e:
         return None
+
 
 if __name__ == "__main__":
     result = do_pack()
