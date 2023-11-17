cc2ea1106b45809377d6ee7f3eff73fb16daa70a
Vlone Me <francisntima@gmail.com>
2023-11-17 19:03:22 +0000

    Set up development with Python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,7 +14,7 @@ A route for the root URL ('/') with strict_slashes=False
 """
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
